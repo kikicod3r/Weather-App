94b79ce7d551b8463d3aab0cd154fae35132ead2
kikicod3r <kitajjenkins@gmail.com>
2023-05-07 12:42:52 -0500

    Added weather icon update based on weather

--- src/javascript.js
+++ src/javascript.js
@@ -54,6 +54,12 @@ function showCurrentCityWeather(response) {
   document.querySelector("#current-temperature-display").innerHTML = Math.round(
     response.data.main.temp
   );
+  document
+    .querySelector("#current-weather-icon")
+    .setAttribute(
+      "src",
+      `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    );
   document.querySelector("#current-weather-description").innerHTML =
     response.data.weather[0].description;
   document.querySelector("#current-humidity").innerHTML =
